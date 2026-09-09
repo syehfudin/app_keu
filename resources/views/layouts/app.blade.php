b4127d395198a4847ffcc7ccf6d908f370ffdcf0
syehfudin <syehfudin@unggulamanah>
2026-09-09 06:34:46 +0000

    fix: restore single h1 page title in layout content-header
    - One h1 title per page from layout (not duplicated in views)
    - Dashboard: 1x 'Dashboard'
    - Transaksi: 1x 'Data Transaksi'
    - Donatur: 1x 'Data Nasabah'
    - Pegawai: 1x 'Data Penghimpun'
    - Setoran: 1x 'Data Setoran'
    - Reha: 1x 'Report Harian'
    - Korel: 1x 'Data Koordinator Penghimpun'
    - Import: 1x 'Import Data'
    - Tunai: 1x 'List Tunai'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
+                        <h1>{{ $title }}</h1>
                     </div>
                 </div>
             </div>
